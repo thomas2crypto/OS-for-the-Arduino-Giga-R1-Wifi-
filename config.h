e157dd0ae197321971825db7c5ec3d8930124fb9
thomas2crypto <julius.baermann@icloud.com>
2025-06-01 13:03:46 +0000

    Update config.h

--- config.h
+++ config.h
@@ -2,10 +2,13 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-//Configuration-bools
+extern "C" char* sbrk(int incr);
+
 extern bool re_setup;
+//Konifguration
 extern bool USBDRIVE;
 extern bool wlan;
+extern bool fixed_wifi;
 extern bool enable_login;
 extern bool startup_config_enabled;
 
